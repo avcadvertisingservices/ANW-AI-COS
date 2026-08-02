1ec89c664a6b791561187aba8542fe4c8959ef35
avcadvertisingservices <hello@avcadvertisingservices.com>
2026-08-02 21:35:17 +0400

    chore: remove duplicate admin portal files from root src

--- src/app/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "./app.js";
-import { handleFatalError } from "../core/errors/index.js";
-
-async function bootstrap(): Promise<void> {
-  const application = new Application();
-  await application.start();
-}
-
-bootstrap().catch(handleFatalError);
